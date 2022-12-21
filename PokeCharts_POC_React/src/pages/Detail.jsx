20656ee5484341a48d75b4e08950a95b93543460
Nomeos <mathieu.rabot01@gmail.com>
2022-12-21 16:12:45 +0100

    Update index.html, package-lock.json, and 3 more files...

--- PokeCharts_POC_React/src/pages/Detail.jsx
+++ PokeCharts_POC_React/src/pages/Detail.jsx
@@ -1,7 +1,71 @@
+import React, {useState} from 'react';
+import {Chart} from 'primereact/chart';
+
 export default function Detail() {
+    const [chartData] = useState({
+        labels: ['HP', 'Att.SPE', 'Def.SPE', 'Speed', 'Def', 'Att'],
+        datasets: [
+            {
+                label: 'My First dataset',
+                backgroundColor: 'rgba(179,181,198,0.2)',
+                borderColor: 'rgba(179,181,198,1)',
+                pointBackgroundColor: 'rgba(179,181,198,1)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgba(179,181,198,1)',
+                data: [65, 59, 90, 81, 56, 55]
+            },
+            {
+                label: 'My Second dataset',
+                backgroundColor: 'rgba(255,99,132,0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                pointBackgroundColor: 'rgba(255,99,132,1)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgba(255,99,132,1)',
+                data: [28, 48, 40, 19, 96, 27]
+            }
+        ]
+    });
+
+    // add intersect option to show the tooltip when the mouse is over the chart
+    const [lightOptions] = useState({
+        plugins: {
+            legend: {
+                labels: {
+                    color: '#495057'
+                }
+            },
+            tooltip: {
+                intersect: false
+            }
+        },
+        scales: {
+            r: {
+                pointLabels: {
+                    color: '#495057',
+                },
+                grid: {
+                    color: '#ebedef',
+                },
+                angleLines: {
+                    color: '#ebedef'
+                }
+            }
+        },
+        interaction: {
+            mode: 'nearest'
+        }
+
+    });
+
     return (
-        <div>
-            <h1>Detail</h1>
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+        }}>
+            <Chart type="radar" data={chartData} options={lightOptions} style={{position: 'relative', width: '40%'}}/>
         </div>
     );
 }
+                 
